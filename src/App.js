4adc59a6b0bd8ac1a4e392e186163800574af262
Nom1L <andrey.sardak.005@mail.ru>
2023-01-05 15:08:18 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
+import ira from './logo.jpeg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+			<img src={ira} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          это Ира
+        </p> 
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vk.com/neenyasho"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Irko
         </a>
+				<img src="https://sun9-west.userapi.com/sun9-1/s/v1/ig2/qY4pbJJXTzdHrtX5SYTBE4vDhyMo7q9x1yGoVNIYyMV1wHK9c2AUIcf618bkiv9yN-T6b7LAXSOo8FR6an3R3mde.jpg?size=1080x1080&quality=95&type=album" className="App-logo" alt="logo" />
       </header>
     </div>
   );
